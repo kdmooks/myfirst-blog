c3b2746780762b38eaba9b81410bd44927417232
Kamogelo <kmookantsa@yahoo.com>
2017-04-29 02:19:16 +0200

    Created all the pages

--- facebookPostsAnalysis/urls.py
+++ facebookPostsAnalysis/urls.py
@@ -1,5 +1,11 @@
 from django.conf.urls import url
 from . import views
 urlpatterns=[
-     url(r'^$',views.patient_Info,name='patient_Info'),
+     url(r'^$',views.homePage,name='homePage'),
+     url(r'^signUpPage/$',views.signUpPage,name='signUpPage'),
+     url(r'^loginPage/$',views.loginPage,name='loginPage'),
+     url(r'^patient_Info/$',views.patient_Info,name='patient_Info'),
+     url(r'^viewPage/$',views.signUpPage,name='viewPage'),
+     url(r'^dataAnalysis/$',views.dataAnalysis,name='dataAnalysis'),
+     url(r'^patientsInfo/$',views.patientsInfo,name='patientsInfo')
 ]
--- facebookPostsAnalysis/views.py
+++ facebookPostsAnalysis/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+def homePage(request):
+	return render(request,'facebookPostsAnalysis/homePage.html', {})
+
+def signUpPage(request):
+	return render(request,'facebookPostsAnalysis/signUpPage.html', {})	
+
+def loginPage(request):
+	return render(request,'facebookPostsAnalysis/loginPage.html', {})	
+
 def patient_Info(request):
-	return render(request,'facebookPostsAnalysis/patient_Info.html', {})
+	return render(request,'facebookPostsAnalysis/patient_Info.html', {})	
+
+def dataAnalysis(request):
+	return render(request,'facebookPostsAnalysis/dataAnalysis.html', {})
+
+def patientsInfo(request):
+	return render(request,'facebookPostsAnalysis/patientsInfo.html', {})				
